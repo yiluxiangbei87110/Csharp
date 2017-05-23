ba4e5e4cd239ee9ec45ccd26857777c167ba63e8
yiluxiangbei87110 <1225459083@qq.com>
2017-05-23 11:54:53 +0800

    c#

--- C#/第一天/Demo9方法中的out参数/Program.cs
+++ C#/第一天/Demo9方法中的out参数/Program.cs
@@ -13,32 +13,33 @@ namespace Demo9方法中的out参数
             //求一个数组的操作
             int[] arr = { 1, 3, 4, 5, 33, 8 };
             int max, min, sum, avg;
-            OutFun(arr, out max, out min, out sum, out avg);
-            Console.WriteLine("最大值是：{0}，最小值是：{1}，总和是：{2}，平均值是：{3}", max, min, sum, avg);
+            bool flag;
+            OutFun(arr, out max, out min, out sum, out avg,out flag);
+            Console.WriteLine("最大值是：{0}，最小值是：{1}，总和是：{2}，平均值是：{3},布尔值是：{4}", max, min, sum, avg,flag);
             Console.ReadKey();
         }
           
-        public static void OutFun(int[] nums,out int max,out int min,out int sum,out int avg)
+        public static void OutFun(int[] nums,out int max1,out int min1,out int sum1,out int avg1,out bool flag1)
         {
-            max = nums[0];
-            min = nums[1];
-            sum = 0;
+            max1 = nums[0];
+            min1 = nums[1];
+            sum1 = 0;
+            flag1 = false;
             for(int i = 0; i < nums.Length; i++)
             {
-                if (max < nums[i])
+                if (max1 < nums[i])
                 {
-                    max = nums[i];
+                    max1 = nums[i];
                 }
-                if (min > nums[i])
+                if (min1 > nums[i])
                 {
-                    min = nums[i];
+                    min1 = nums[i];
                 }
-                sum += nums[i];
+                sum1 += nums[i];
             }
-            avg = sum / nums.Length;
+            avg1 = sum1 / nums.Length;
         }
        
     }
 }
-
 
