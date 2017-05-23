f5987f517375ecd7aae62bbf4f26475a8522636f
yiluxiangbei87110 <1225459083@qq.com>
2017-05-23 15:21:07 +0800

    c# out params ref 参数

---
+++ C#/第一天/Demo9方法params可变参数/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo9方法params可变参数
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //params可变参数，减少了变量。但是注意只能有一个可变参数，并且只能放在最后。
+            Test("Felix",89,90,97);
+            Console.ReadKey();
+        }
+
+        public static void Test(string name,params int[] nums)
+        {
+            int sum = 0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+            }
+            Console.WriteLine("{0}的成绩是{1}", name, sum);
+        }
+    }
+}
---
+++ C#/第一天/Demo9方法中的ref参数/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo9方法中的ref参数
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salary = 5000.12;
+            //1虽然调用，但是并不影响这个代码块里salary的值
+            Salary(salary);
+            Console.WriteLine(salary);//5000.12 调用了，但是此方法没有返回值
+            Console.ReadKey();
+
+            //2又返回值，正常使用
+            double s2 = Salary2(salary);
+            Console.WriteLine(s2);
+            Console.ReadKey();
+
+            //3使用ref参数，解决第一种情况下的问题
+            Salary3(ref salary);
+            Console.WriteLine(salary);
+            Console.ReadKey();
+            int n1 = 100;int n2 = 10;
+            ChangValue(ref n1, ref n2);
+            Console.WriteLine("现在n1是：{0}，现在n2是：{1}", n1, n2);//10 100
+            Console.ReadKey();
+        }
+        public static void Salary(double s)
+        {
+            s += 500;
+        }
+        public static double Salary2(double s)
+        {
+           return  s += 500;
+        }
+        public static void Salary3(ref double s)
+        {
+            s += 5000;
+        }
+        //ref 参数小的例子 完成交换
+        public static void ChangValue(ref int a,ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+    }
+}
+
+
