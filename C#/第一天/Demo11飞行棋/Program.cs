4c4a4a9eaa6547d22d83b0dd7a140e313cd25c6a
yiluxiangbei87110 <1225459083@qq.com>
2017-05-23 22:34:25 +0800

    飞行棋

---
+++ C#/第一天/Demo11飞行棋/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo11飞行棋
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameShow();
+        }
+        //游戏头部
+        public static void GameShow()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("****************");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("****************");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("****************");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("****Felix的飞行棋*****");
+            Console.WriteLine("****************");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("****************");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("****************");
+            Console.ReadKey();
+        }
+    }
+}
