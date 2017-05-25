7a34050ae5c0f8ed462fc27bbda506f273915791
yiluxiangbei87110 <1225459083@qq.com>
2017-05-25 09:18:00 +0800

    int 12 / double 12.0 =1

--- C#/第一天/Test类型转换/Program.cs
+++ C#/第一天/Test类型转换/Program.cs
@@ -10,40 +10,45 @@ namespace Test类型转换
     {
         static void Main(string[] args)
         {
-            //隐式转换和显式转换（double类型放置int类型是可以的，隐式转换，但是反过来，int类型不能直接放置double类型的格式，此时显式转换）
-            double a = 3.141592653;
-            int b = (int)a;
-            Console.WriteLine(b);
-            Console.ReadLine();
-            //隐式转换
-            int c = 123;
-            double d = c;//没问题
+            ////隐式转换和显式转换（double类型放置int类型是可以的，隐式转换，但是反过来，int类型不能直接放置double类型的格式，此时显式转换）
+            //double a = 3.141592653;
+            //int b = (int)a;
+            //Console.WriteLine(b);
+            //Console.ReadLine();
+            ////隐式转换
+            //int c = 123;
+            //double d = c;//没问题
 
-            //c#中提供了多种的转换方法 
-            Console.WriteLine(d.ToString());
-            //下面这些转化没有成功？？？？？？
-            //Console.WriteLine(d.ToByte())
-            //Console.WriteLine(d.ToInt16());
-            //Console.WriteLine(d.ToInt32());
-            //Console.WriteLine(d.ToDateTime());
-            //Console.WriteLine(d.ToDecimal());
-            //Console.WriteLine(d.ToChar());
-            //Console.WriteLine(d.UInt64());
+            ////c#中提供了多种的转换方法 
+            //Console.WriteLine(d.ToString());
+            ////下面这些转化没有成功？？？？？？
+            ////Console.WriteLine(d.ToByte())
+            ////Console.WriteLine(d.ToInt16());
+            ////Console.WriteLine(d.ToInt32());
+            ////Console.WriteLine(d.ToDateTime());
+            ////Console.WriteLine(d.ToDecimal());
+            ////Console.WriteLine(d.ToChar());
+            ////Console.WriteLine(d.UInt64());
 
-            string locstr ="7891";
-            //如果要将"locstr"转成整型数
-            //方法一: 用 Convert 
-            int i = Convert.ToInt16(locstr);
-            //方法二: 用 Parse
-            int ii = int.Parse(locstr);
-            Console.WriteLine(i);//123
-            Console.WriteLine(ii);//7891
+            //string locstr ="7891";
+            ////如果要将"locstr"转成整型数
+            ////方法一: 用 Convert 
+            //int i = Convert.ToInt16(locstr);
+            ////方法二: 用 Parse
+            //int ii = int.Parse(locstr);
+            //Console.WriteLine(i);//123
+            //Console.WriteLine(ii);//7891
 
-            int num = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("您刚刚输入的是数字是{0}", num);
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("您刚刚输入的是数字是{0}", num);
 
-            const double aa = 3.145;
-            Console.WriteLine(aa);
+            //const double aa = 3.145;
+            //Console.WriteLine(aa);
+            //Console.ReadKey();
+            int a = 12;
+            double b = 12.0;
+            double c = a / b;
+            Console.WriteLine(c);//1
             Console.ReadKey();
         }
     }
