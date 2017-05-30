d275797236b3687228f987874e0302c71bfecab1
yiluxiangbei87110 <1225459083@qq.com>
2017-05-30 23:27:59 +0800

    泛型

--- C#/第二天/Demo6Hashtable键值对集合/Program.cs
+++ C#/第二天/Demo6Hashtable键值对集合/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace Demo6Hashtable键值对集合
 {
@@ -10,6 +11,22 @@ namespace Demo6Hashtable键值对集合
     {
         static void Main(string[] args)
         {
+            Hashtable ht = new Hashtable();
+            ht.Add("name", "Felix");
+            ht.Add(2, "zhngsan");
+            ht.Add(false, "cuode");
+            ht.Remove(false);//删掉指定的键
+            foreach (var item in ht.Keys)
+            {
+                Console.WriteLine(ht[item]);
+            }
+            if (ht.ContainsKey("name"))
+            {
+                Console.WriteLine("已经包含了做name");
+            }
+            ht.Clear();//清除全部
+            Console.ReadKey();
+
         }
     }
 }
