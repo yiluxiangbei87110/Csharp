9ffe450341221e4820e6d85097b5d45a0b68da7b
yiluxiangbei87110 <1225459083@qq.com>
2017-06-02 20:20:53 +0800

    多态

---
+++ C#/第二天/Demo9复制文件/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo9复制文件
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //复制文件，其实就是先读后写
+            string source = @"D:\huahai.mp4";
+            string target = @"D:\huahai2.mp4";
+            CopyFile(source,target);
+            Console.WriteLine("复制ok");
+            Console.ReadKey();
+        }
+        public static void CopyFile(string source,string target)
+        {
+            //创建一个读取的流
+            using (FileStream fsRead = new FileStream(source, FileMode.Open, FileAccess.Read))
+            {
+                //创建一个写入的流
+                using(FileStream fsWrite=new FileStream(target,FileMode.OpenOrCreate,FileAccess.Write))
+                {
+                    byte[] buffer = new byte[1024*1024*5];
+
+                    //文件比较大，可以通过循环读
+                    while(true){
+                        //返回本次实际读到的字节数
+                        int r = fsRead.Read(buffer, 0, buffer.Length);
+                        //当返回的是0的时候，我们知道已经读取完了
+                        if (r == 0)
+                        {
+                            break;
+                        }
+                        //注意后面的r（最后一次可能不是整数）
+                        fsWrite.Write(buffer, 0, r);
+                    }
+                   
+                }
+            }
+        }
+    }
+}
---
+++ C#/第二天/demo91抽象/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo91抽象
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //狗叫 猫也叫  无法实例化抽象或借口类
+            //Animal a = new Animal();
+            //虽然赋给了父类，但是其子类重写了父类的抽象方法，所以依然是子类的方法
+            Animal a = new Dog();
+            a.Bark();
+        }
+    }
+    public abstract  class  Animal
+    {
+        //抽象方法不允许有方法体
+        public abstract void Bark();
+    }
+
+    public class Dog : Animal
+    {
+        public override void Bark()
+        {
+            Console.WriteLine("狗狗汪汪叫");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void Bark()
+        {
+            Console.WriteLine("猫猫喵喵叫");
+        }
+    }
+
+
+}
