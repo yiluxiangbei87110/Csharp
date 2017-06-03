1ef38b93fbd6c011bb8c6c71039401feab50b2fb
yiluxiangbei87110 <1225459083@qq.com>
2017-06-03 17:47:20 +0800

    接口

---
+++ C#/第二天/Demo95接口/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo95接口
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Flyable f = new Bird();//new Person();
+            f.Fly();
+            Console.ReadKey();
+        }
+    }
+    
+    public interface Flyable
+    {
+        //成员不能含有修饰符 并且没有方法体
+        void Fly();
+
+    }
+    public class Person : Flyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("人类可以fly");
+        }
+    }
+    public class Bird : Flyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("鸟类也可以fly");
+        }
+    }
+}
