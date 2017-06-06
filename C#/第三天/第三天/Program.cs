01dfe2a5cda630003ed57e77631c6e0d68d61103
yiluxiangbei87110 <1225459083@qq.com>
2017-06-06 22:09:47 +0800

    md5

---
+++ C#/第三天/第三天/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 第三天
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public static string GetMd5( string str)
+        {
+            //创建md5
+            MD5 md5 = MD5.Create();
+        }
+    }
+}
