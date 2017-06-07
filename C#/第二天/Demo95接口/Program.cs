157d1218cdd67f7d7ceff90541cbb3471d169772
yiluxiangbei87110 <1225459083@qq.com>
2017-06-07 20:42:28 +0800

    接口

--- C#/第二天/Demo95接口/Program.cs
+++ C#/第二天/Demo95接口/Program.cs
@@ -18,19 +18,36 @@ namespace Demo95接口
     
     public interface Flyable
     {
-        //成员不能含有修饰符 并且没有方法体
+        //成员不能含有修饰符默认就是public 并且没有方法体
         void Fly();
+        //string _name; error:接口不能含有字段
+        String Name
+        {
+            //属性中不可以有方法体 自动属性
+            set;
+            get;
+        }
 
     }
     public class Person : Flyable
-    {
+    {   
         public void Fly()
         {
             Console.WriteLine("人类可以fly");
         }
+        public String Name
+        {
+            set { Name = value; }
+            get { return Name; }
+        }
     }
     public class Bird : Flyable
     {
+        public String Name
+        {
+            set { Name = value; }
+            get { return Name; }
+        }
         public void Fly()
         {
             Console.WriteLine("鸟类也可以fly");
