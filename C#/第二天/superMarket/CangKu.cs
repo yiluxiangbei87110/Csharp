6a1a1146ccb0d7ba0e0e7d7d0ac9134e3c5e2aa2
yiluxiangbei87110 <1225459083@qq.com>
2017-06-07 23:27:19 +0800

    supermarket

---
+++ C#/第二天/superMarket/CangKu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace superMarket
+{
+    class CangKu
+    {
+        //存储货物
+        //List<SumSung> listS = new List<SumSung>();
+        //List<JiangYou> listJ = new List<JiangYou>();
+        //List<Banana> listB = new List<Banana>();
+        //List<Acer> listA = new List<Acer>();
+        //上面太繁琐
+        List<ProductFather> list = new List<ProductFather>();
+
+    }
+}
---
+++ C#/第二天/superMarket/JiangYou.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace superMarket
+{
+    class JiangYou : ProductFather
+    {
+        public JiangYou(string id, double price, double count) : base(id, price, count)
+        {
+
+        }
+    }
+}
