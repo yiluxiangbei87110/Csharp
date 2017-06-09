b283983d3017223b1d3e8736b96106d0fac53430
yiluxiangbei87110 <1225459083@qq.com>
2017-06-09 15:31:35 +0800

    c#

--- extjs/calc/app.js
+++
@@ -1,8 +0,0 @@
-
-Ext.application({
-    name: 'Calc',    
-    launch: function () {
-        Ext.create('Calc.view.main.Main').show();
-        //Will create the calculator as a floating, movable window within the browser 
-    }
-});
