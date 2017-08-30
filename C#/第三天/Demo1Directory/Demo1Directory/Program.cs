60eeccc175c96ebfe80197c02685e936de801b5f
yiluxiangbei87110 <1225459083@qq.com>
2017-08-30 23:20:22 +0800

    a

---
+++ C#/第三天/Demo1Directory/Demo1Directory/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1Directory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Directory 创建一个a文件夹
+            Directory.CreateDirectory(@"C:\a");
+            Console.WriteLine("创建成功");
+            Console.ReadKey();
+
+            //删除的文件夹下不能有文件
+            Directory.Delete(@"C:\a");
+            //如果全部删掉需要一个布尔值true
+            //Directory.Delete(@"C:\a",true);
+            Console.WriteLine("删除了");
+            Console.ReadKey();
+
+            //剪切操作，包括文件夹下的所有文件都被剪切到了桌面位置
+            Directory.Move(@"C:\b", @"C:\Users\Administrator\Desktop\new");
+            Console.WriteLine("剪切成功");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ C#/第三天/Demo1Directory/Demo2Process/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo2Process
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] pg = Process.GetProcesses();
+            foreach (var item in pg)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+            //通过进程打开一些程序(下面打开计算器)
+            Process.Start("calc");
+            Process.Start("mspaint");
+            Process.Start("notepad");
+            Process.Start("iexplore", "http://www.baidu.com");
+
+            //process打开一个文件
+            ProcessStartInfo psi = new ProcessStartInfo(@"C:\Users\Administrator\Desktop\新建文本文档.txt");
+            Process ps = new Process();
+            ps.StartInfo = psi;
+            ps.Start();
+        }
+    }
+}
