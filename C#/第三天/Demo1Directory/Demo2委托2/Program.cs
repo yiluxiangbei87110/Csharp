1ec50831b24bb561e37905d90190975e6ad78462
yiluxiangbei87110 <1225459083@qq.com>
2017-09-03 22:44:19 +0800

    preview

---
+++ C#/第三天/Demo1Directory/Demo2委托2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo2委托2
+{   //委托声明注意是卸载命名空间下，而且形式和下面的函数一致。
+    public delegate void DELSayHi(string name);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //创建一个委托对象
+            //DELSayHi del = new DELSayHi(ChiSayHello);
+            //del("zhangsan");
+            //可以简单点，直接调用
+            Test("zhagnsan", ChiSayHello);
+
+            //更简单点
+            DELSayHi del = delegate (string name)
+              {
+                  Console.Write("你好，" + name);
+              };
+
+            //最简单
+            //DELSayHi del =(string name)=>{
+            //    Console.Write("你好，" + name);
+            //};
+            Console.ReadKey();
+
+        }
+
+        public static void Test(string name,DELSayHi del)
+        {
+            //调用
+            del(name);
+
+        }
+        public static void ChiSayHello(string name)
+        {
+            Console.WriteLine("吃了吗,"+name);
+        }
+        public static void EngSayHello(string name) 
+        {
+            Console.WriteLine("HI,"+name);
+        }
+    }
+}
---
+++ C#/第三天/Demo1Directory/Demo3委托/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo3委托
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "fjkASJKdd", "hfdjfhaoeui", "FFF" };
+            AddSYH(names);
+            for(int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.ReadKey();
+           
+        }
+        //操作的是引用类型，不需要返回值。
+        //三个方法类似，我们可以使用委托。
+        public static void ToUpperCase(string[] name)
+        {
+            for(int i = 0; i < name.Length; i++)
+            {
+                name[i] = name[i].ToUpper();
+            }
+        }
+        public static void ToLowerCase(string[] name)
+        {
+            for(var i = 0; i < name.Length; i++)
+            {
+                name[i] = name[i].ToLower();
+            }
+        }
+        public static void AddSYH(string[] name)
+        {
+            for(int i = 0; i < name.Length; i++)
+            {
+                name[i] = "\"" + name[i] + "\"";
+            }
+        }
+    }
+}
+   
