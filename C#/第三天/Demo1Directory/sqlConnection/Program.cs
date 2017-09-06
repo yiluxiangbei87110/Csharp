51886d5a0b6911bb4d3060c6fe5200879d503fb5
yiluxiangbei87110 <1225459083@qq.com>
2017-09-06 23:21:01 +0800

    连接数据库

---
+++ C#/第三天/Demo1Directory/sqlConnection/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace sqlConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connStr = "server=127.0.0.1;uid=sa;pwd=123456,database=DB1";
+            SqlConnection con = new SqlConnection(connStr);
+
+            con.Open();
+            Console.WriteLine("数据库打开");
+            con.Close();
+            con.Dispose();
+            Console.ReadKey();
+        }
+    }
+}
