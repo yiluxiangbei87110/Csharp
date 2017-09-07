1ccfb09876bf6cc9cb25c243a2f7eb2c5369806d
yiluxiangbei87110 <1225459083@qq.com>
2017-09-07 23:48:31 +0800

    写入数据库

--- C#/第三天/Demo1Directory/sqlConnection/Program.cs
+++ C#/第三天/Demo1Directory/sqlConnection/Program.cs
@@ -10,13 +10,32 @@ namespace sqlConnection
     {
         static void Main(string[] args)
         {
-            string connStr = "server=127.0.0.1;uid=sa;pwd=123456,database=DB1";
-            SqlConnection con = new SqlConnection(connStr);
+            //01 连接数据库
+            //#region
+            //链接数据库 uid用户名 pwd 密码
+            //string connStr = "server=127.0.0.1;uid=sa;pwd=123456;database=DB1";
+            //SqlConnection con = new SqlConnection(connStr);
 
+            //con.Open();
+            //Console.WriteLine("数据库打开");
+            //con.Close();
+            //con.Dispose();
+            //#endregion
+
+            //02写入数据库内容
+            #region
+            //1.创建对象，连接数据库
+            string conStr = "Server=(local);uid=sa;pwd=123456;database=DB1";
+            SqlConnection con = new SqlConnection(conStr);
+            //创建一个sql命令对象
+            SqlCommand cmd = new SqlCommand();
+            //执行命令前打开open命令
             con.Open();
-            Console.WriteLine("数据库打开");
-            con.Close();
-            con.Dispose();
+            cmd.Connection = con;
+            cmd.CommandText = "insert into dbo.category(catName,parentCatId,delFlag)values('chicken',1,0)";
+            //执行非查询语句，返回受影响的行数
+            cmd.ExecuteNonQuery();
+            #endregion
             Console.ReadKey();
         }
     }
